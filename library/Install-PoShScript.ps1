deffcba0de548bacc9016050f233c8d03f80ac6f
Fabien Sanchez <fzed51+git@gmail.com>
2022-06-21 22:22:24 +0200

    fix / pour la retrocompatibilite

---
+++ library/Install-PoShScript.ps1
@@ -0,0 +1,64 @@
+$URL = "https://fzed51.github.io/powershell-library"
+$handleWeb = Invoke-WebRequest ($URL + "/script_catalog.json")
+$catalog = ($handleWeb.content | ConvertFrom-Json)
+
+$RegistryLIbrary = $URL + "/library/scripts/"
+$PowershellDirectory = Split-Path $profile
+$PowershellScriptDirectory = Join-Path $PowershellDirectory "Scripts"
+$InstalledScriptFile = Join-Path $PowershellDirectory "installed-script.json"
+
+$Index = 1
+$catalog | ForEach-Object {
+    Write-Host ("{0} - {1}" -f $Index, $_.name)
+    $Index ++
+}
+
+$Rep = Read-Host "No du script à installer"
+if ($Rep -notmatch "\d+") {
+    # FIN DU SCRIPT
+    return ;
+}
+[int]$NScript = $Rep
+
+$Script = $catalog[$NScript - 1];
+Write-Host ("Vous allez installer {0}" -f $Script.name) 
+
+if (-not (Test-Path $PowershellScriptDirectory -PathType Container)) {
+    New-Item $PowershellScriptDirectory -Force | Out-Null
+}
+# Test du dossier scripts dans la variable d'environement Path
+[string[]]$Path = `
+([System.Environment]::GetEnvironmentVariable('PATH', 'user')).split(';') `
+| ForEach-Object { $_.TrimEnd('/\') }
+if ($Path -notcontains $PowershellScriptDirectory.TrimEnd('/\')) {
+    [System.Environment]::SetEnvironmentVariable('PATH', (
+        ($Path -join ";") + ";" + $PowershellScriptDirectory
+        ), 'user')
+    Write-Host ("Le dossier {0} a été enregistré dans les variables d'environement" -f $PowershellScriptDirectory)
+}
+
+Invoke-WebRequest ($RegistryLIbrary + $Script.name ) -OutFile (Join-Path $PowershellScriptDirectory $Script.name)
+
+if (Test-Path $InstalledScriptFile -PathType Leaf) {
+    $Installed = Get-Content $InstalledScriptFile | ConvertFrom-Json
+    if ($Null -eq $Installed) {
+        $Installed = @()
+    }
+    if ($Installed.GetType().Name -eq "PSCustomObject") {
+        $Installed = @($Installed)
+    } 
+    $Installed = $Installed | Where-Object { $_.id -ne $Script.id }
+    if ($Null -eq $Installed) {
+        $Installed = @()
+    }
+    if ($Installed.GetType().Name -eq "PSCustomObject") {
+        $Installed = @($Installed)
+    } 
+
+    $Installed = $Installed + $Script
+
+    $Installed | ConvertTo-Json | Set-Content $InstalledScriptFile -Encoding ascii
+}
+else {
+    Write-Error "Un problème est survenu lors du téléchargement du fichier" -Category ResourceUnavailable -ErrorAction Stop
+}
---
+++ library/Update-PoShScript.ps1
@@ -0,0 +1,64 @@
+$RegistryLIbrary = "https://fzed51.github.io/powershell-library/library/"
+$PowershellDirectory = Split-Path $profile
+$PowershellScriptDirectory = Join-Path $PowershellDirectory "Scripts"
+$InstalledScriptFile = Join-Path $PowershellDirectory "installed-script.json"
+
+# RECUPERATION DU CATALOGUE
+$handleWeb = wget "https://fzed51.github.io/powershell-library/catalog.json"
+$catalog = ($handleWeb.content | ConvertFrom-Json)
+
+# RECUPERATION DES SCRIPTS INSTALLES
+$Installed = Get-Content $InstalledScriptFile | ConvertFrom-Json
+if ($Null -eq $Installed) {
+    $Installed = @()
+}
+if ($Installed.GetType().Name -eq "PSCustomObject") {
+    $Installed = @($Installed)
+}
+
+function updateScript {
+    Param(
+        $Old, 
+        $New,
+        $Collection
+    )
+
+    $Temps = New-Guid
+    Rename-Item -Path (Join-Path $PowershellScriptDirectory $Old.name) -NewName $Temps
+    
+    $Collection = $Collection | Where-Object { $_.id -ne $Old.id }
+    if ($Null -eq $Collection) {
+        $Collection = @()
+    }
+    if ($Collection.GetType().Name -eq "PSCustomObject") {
+        $Collection = @($Collection)
+    }
+
+    try {
+        Invoke-WebRequest ($RegistryLIbrary + $New.name ) -OutFile (Join-Path $PowershellScriptDirectory $New.name)
+        Remove-Item (Join-Path $PowershellScriptDirectory $Temps)
+        Write-Host ("{0} a été mis à jour " -f $Old.name) -NoNewline
+        Write-Host $Old.version -NoNewline -ForegroundColor DarkGreen
+        Write-Host  " -> " -NoNewline 
+        Write-Host $New.version -NoNewline -ForegroundColor Green
+        $Collection = $Collection + $New
+    }
+    catch {
+        Rename-Item -Path (Join-Path $PowershellScriptDirectory $Temps) -NewName $Old.name
+        $Collection = $Collection + $Old
+    }
+
+    return $Collection
+}
+
+$NewInstalled = $Installed.Clone()
+$Installed | ForEach-Object {
+    $InstalledScript = $_
+    $catalog `
+    | Where-Object { $_.id -eq $InstalledScript.id -and $_.version -ne $InstalledScript.version } `
+    | ForEach-Object {
+        $NewInstalled = updateScript -Old $InstalledScript -New $_ -Collection $NewInstalled
+    }
+}
+
+$NewInstalled | ConvertTo-Json | Set-Content $InstalledScriptFile -Encoding ascii
