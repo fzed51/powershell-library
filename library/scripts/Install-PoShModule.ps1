f43c07ff47bfdde7b53aa19f6a6ada9857523681
Fabien Sanchez <fzed51+git@gmail.com>
2022-06-23 11:13:21 +0200

    feat / installation des module nouvelle methode

--- library/scripts/Install-PoShModule.ps1
+++ library/scripts/Install-PoShModule.ps1
@@ -2,6 +2,19 @@ $URL = "https://fzed51.github.io/powershell-library"
 $handleWeb = Invoke-WebRequest ($URL + "/modules_catalog.json")
 $catalog = ($handleWeb.content | ConvertFrom-Json)
 
+function Join-Url {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [String]
+        $Parent,
+        [Parameter()]
+        [String]
+        $Child
+    )
+    return $Parent.TrimEnd('/') + '/' + $Child.TrimStart('/')
+}
+
 $RegistryLIbrary = $URL + "/library/modules/"
 $PowershellDirectory = Split-Path $profile
 $PowershellModuleDirectory = Join-Path $PowershellDirectory "Modules"
@@ -37,7 +50,35 @@ if ($EnvPSModulePath -notcontains $PowershellModuleDirectory.TrimEnd('/\')) {
     Write-Host ("Le dossier {0} a été enregistré dans les variables d'environement" -f $PowershellModuleDirectory)
 }
 
-Invoke-WebRequest ($RegistryLIbrary + $Module.name ) -OutFile (Join-Path $PowershellModuleDirectory $Module.name)
+# CREATION DU DOSSIER 
+$DirectoryModule = Join-Path $PowershellModuleDirectory $Module.name
+$UrlModule = Join-Url $RegistryLIbrary  $Module.name
+
+if (Test-Path  $DirectoryModule -PathType Container) {
+    $ErrorMsg = "Le module {0} semble déjà être installé" -f $Module.name
+    Write-Error -ErrorAction Stop -Category OperationStopped $ErrorMsg
+}
+New-Item  $DirectoryModule -Type Directory -ErrorAction SilentlyContinue
+$Module.files | ForEach-Object {
+    $DirectoryModuleFile = Join-Path $DirectoryModule (Split-Path $_)
+    If ($DirectoryModuleFile -ne '' -and (-not (Test-Path $DirectoryModuleFile))) {
+        New-Item $DirectoryModuleFile -Type Directory 
+    }
+    $UrlFile = Join-Url $UrlModule $_
+    $PathFile = Join-Path $DirectoryModule $_
+    Write-Host  $UrlFile, '->' , $PathFile
+    Invoke-WebRequest $UrlFile -OutFile  $PathFile -ErrorAction Stop
+}
+New-ModuleManifest `
+    -Path (Join-Path $DirectoryModule ("{0}.psd1" -f $Module.name) ) `
+    -Guid $Module.id `
+    -Author 'fzed51' `
+    -CompanyName 'none' `
+    -ModuleVersion $Module.version `
+    -FileList $Module.files `
+    -RootModule $Module.files[0] `
+    -FunctionsToExport $Module.functionsn
+
 
 if (Test-Path $InstalledModuleFile -PathType Leaf) {
     $Installed = Get-Content $InstalledModuleFile | ConvertFrom-Json
@@ -47,18 +88,19 @@ if (Test-Path $InstalledModuleFile -PathType Leaf) {
     if ($Installed.GetType().Name -eq "PSCustomObject") {
         $Installed = @($Installed)
     } 
-    $Installed = $Installed | Where-Object { $_.id -ne $Module.id }
-    if ($Null -eq $Installed) {
-        $Installed = @()
-    }
-    if ($Installed.GetType().Name -eq "PSCustomObject") {
-        $Installed = @($Installed)
-    } 
-
-    $Installed = $Installed + $Module
-
-    $Installed | ConvertTo-Json | Set-Content $InstalledModuleFile -Encoding ascii
 }
 else {
-    Write-Error "Un problème est survenu lors du téléchargement du fichier" -Category ResourceUnavailable -ErrorAction Stop
+    $Installed = @()
 }
+
+$Installed = $Installed | Where-Object { $_.id -ne $Module.id }
+if ($Null -eq $Installed) {
+    $Installed = @()
+}
+if ($Installed.GetType().Name -eq "PSCustomObject") {
+    $Installed = @($Installed)
+} 
+
+$Installed = $Installed + $Module
+
+$Installed | ConvertTo-Json | Set-Content $InstalledModuleFile -Encoding ascii
